c60022593c2698db1b4ca1111adb9d69c45235ed
oguzcanasidev <turanoguzcan615@gmail.com>
2025-08-05 15:02:29 +0300

    user ve auth modülü init edildi. auth için gerekli paketler yüklendi jwt,bcryptjs ve passport

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(
       process.env.MONGO_URI || 'mongodb+srv://xxx:xxx@xxx.xxx.mongodb.net/xxx'
-    ), // MongoDB bağlantı URI
+    ),
+    UsersModule,
+    AuthModule, // MongoDB bağlantı URI
   ],
   controllers: [AppController],
   providers: [AppService],
