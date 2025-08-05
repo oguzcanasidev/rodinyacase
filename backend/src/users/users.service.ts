8e310459587901e55a6ba6199e7acad977e6880e
oguzcanasidev <turanoguzcan615@gmail.com>
2025-08-05 15:09:29 +0300

    user modülü için email ve username den bulma fonksiyonları eklendi. create eklendi.

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async create(email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    // Email'deki @ öncesini alıp username yapıyoruz
+    const username = email.split('@')[0];
+  
+    const newUser = new this.userModel({ email, username, password: hashedPassword });
+    return newUser.save();
+  }
+}
