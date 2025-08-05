7010a2bf713b82c2044d9634f4cb63a67813b514
oguzcanasidev <turanoguzcan615@gmail.com>
2025-08-05 16:06:04 +0300

    şema type hatası düzeltildi

--- backend/src/users/user.schema.ts
+++ backend/src/users/user.schema.ts
@@ -14,7 +14,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: false, default: null })
+  @Prop({ type: String, required: false, default: null })
   refreshToken: string | null;
 }
 
